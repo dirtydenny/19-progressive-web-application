0dbf138e7ef9725782a1bce75402bc45cccc0900
dirtydenny <dallen7@socal.rr.com>
2023-04-20 11:02:40 -0700

    adding to database.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -6,7 +6,6 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // Store the triggered events
     window.deferredPrompt = event;
-
     // Remove the hidden class from the button.
     butInstall.classList.toggle('hidden', false);
   });
@@ -18,10 +17,8 @@ butInstall.addEventListener('click', async () => {
   if (!promptEvent) {
     return;
   }
-
   // Show prompt
   promptEvent.prompt();
-
   // Reset the deferred prompt variable, it can only be used once.
   window.deferredPrompt = null;
 

8c50f40c9969e2d40793fe033d792f00371ec4b2
dirtydenny <dallen7@socal.rr.com>
2023-04-20 15:49:07 -0700

    working better

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
    // Open up the desired object store.
    const store = tx.objectStore("jate");
    // Use the .add() method on the store and pass in the content.
-   const request = store.put({ id: id, jate: content  
+   const request = store.put({ id: 1, value: content  
    });
    // Get confirmation of the request.
    const result = await request;
@@ -48,7 +48,7 @@ export const getDb = async () => {
   const request = store.get(1);
   // Get confirmation of the request.
   const result = await request;
-  console.log("result.value", result.value);
+  console.log("result.value", result);
   return result;
 }
 

13de0062547c37ae0e32ec61e20b8a69fcdece18
dirtydenny <dallen7@socal.rr.com>
2023-04-20 10:39:58 -0700

    initial commit, already working

---
+++ client/src/js/database.js
@@ -0,0 +1,24 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  // We are creating a new database named 'jate' which will be using version 1 of the database.
+  openDB("jate", 1, {
+    // Add our database schema if it has not already been initialized.
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
+        return;
+      }
+      // Create a new object store for the data and give it an key name of 'id' which needs to increment automatically.
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
+    },
+  });
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => console.error('putDb not implemented');
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => console.error('getDb not implemented');
+
+initdb();
